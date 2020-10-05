3b5af2f0fdf4d272fb492ce402525eac72563d29
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-10-04 20:25:45 -0600

    created app logic

--- Index.js
+++ Index.js
@@ -1,6 +1,6 @@
-const Employee = require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
+const App = require('./lib/App');
+const Inquirer = require('inquirer');
 
+const app = new App();
 
+app.start();
